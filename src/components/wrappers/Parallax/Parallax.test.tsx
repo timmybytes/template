1a6b63c5225acdb3b1ad525b26dffd67e569e9b1
Timothy Merritt <timmybytes@gmail.com>
2022-03-08 14:59:38 -0800

    test: Update parallax test

--- src/components/wrappers/Parallax/Parallax.test.tsx
+++ src/components/wrappers/Parallax/Parallax.test.tsx
@@ -3,6 +3,14 @@ import { Parallax } from './Parallax'
 
 describe('Parallax', () => {
   it('should render', () => {
-    render(<Parallax />)
+    render(
+      <Parallax>
+        {' '}
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus vero,
+        reiciendis quas culpa veniam, laudantium aut sequi placeat inventore
+        quibusdam non exercitationem quo repellendus dignissimos odit fuga
+        suscipit ea dolorum?
+      </Parallax>
+    )
   })
 })
