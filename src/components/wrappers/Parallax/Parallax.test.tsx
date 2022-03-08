d306774bd33b002c60d8fcbf964f2b263111df11
Timothy Merritt <timmybytes@gmail.com>
2022-03-08 14:58:34 -0800

    chore: Add parallax

---
+++ src/components/wrappers/Parallax/Parallax.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@test/test-utils'
+import { Parallax } from './Parallax'
+
+describe('Parallax', () => {
+  it('should render', () => {
+    render(<Parallax />)
+  })
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
+import Image from 'next/image'
 import { Heading } from '@components/atoms/Heading'
 import { Header } from '@components/organisms/Header'
+import { Parallax } from '@components/wrappers/Parallax'
 import React from 'react'
 import tw from 'twin.macro'
 
@@ -27,6 +29,16 @@ export default function Home(): React.ReactNode {
           quibusdam non exercitationem quo repellendus dignissimos odit fuga
           suscipit ea dolorum?
         </Text>
+        <Heading customCss={tw`font-extrabold text-4xl`}>
+          Parallax (with spring)
+        </Heading>
+        <Parallax spring offset={-100}>
+          <img
+            src='https://images.unsplash.com/photo-1646734104317-4bcd21298b3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+            alt='coffee'
+            tw='max-width[20vw]'
+          />
+        </Parallax>
       </Main>
     </div>
   )
@@ -34,5 +46,5 @@ export default function Home(): React.ReactNode {
 
 const Main = tw.main`
   flex flex-col justify-center items-center
-  w-full height[80vh]`
+  w-full height[180vh]`
 const Text = tw.p`max-width[65ch] p-4`
