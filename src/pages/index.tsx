6cda974d9aa602869e13ee9db58ad695a3126462
Timothy Merritt <timmybytes@gmail.com>
2022-05-20 10:39:07 -0700

    chore: Update Logo --WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,16 @@
-import Image from 'next/image'
 import { Heading } from '@components/atoms/Heading'
 import { Header } from '@components/organisms/Header'
-import { Parallax } from '@components/wrappers/Parallax'
 import React from 'react'
 import tw from 'twin.macro'
 
 export default function Home(): React.ReactNode {
   return (
-    <div tw='flex flex-col justify-center items-center'>
+    <div
+      tw='flex flex-col justify-start
+     items-center'
+    >
       <Header />
-      <Main tw='relative'>
+      <Main>
         <Heading customCss={tw`font-extrabold text-4xl`}>Some Text</Heading>
         <Text>
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus vero,
@@ -29,22 +30,13 @@ export default function Home(): React.ReactNode {
           quibusdam non exercitationem quo repellendus dignissimos odit fuga
           suscipit ea dolorum?
         </Text>
-        <Heading customCss={tw`font-extrabold text-4xl`}>
-          Parallax (with spring)
-        </Heading>
-        <Parallax spring offset={-100}>
-          <img
-            src='https://images.unsplash.com/photo-1646734104317-4bcd21298b3c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
-            alt='coffee'
-            tw='max-width[20vw]'
-          />
-        </Parallax>
       </Main>
+      <div>W</div>
     </div>
   )
 }
 
 const Main = tw.main`
-  flex flex-col justify-center items-center
-  w-full height[180vh]`
+  flex flex-col justify-start items-center
+  w-full h-full`
 const Text = tw.p`max-width[65ch] p-4`
